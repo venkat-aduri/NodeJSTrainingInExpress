46f92e5a26facf317f2a3c658b45859a73701a9c
venkat aduri <venkatreactjs369@gmail.com>
2023-07-02 12:27:18 +0530

    mongodb connection

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mongoUrl = 'mongodb://venkatreactjs:Venky7799@ac-tobec1f-shard-00-00.q315mof.mongodb.net:27017,ac-tobec1f-shard-00-01.q315mof.mongodb.net:27017,ac-tobec1f-shard-00-02.q315mof.mongodb.net:27017/?ssl=true&replicaSet=atlas-y586fq-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+//mongodb connection
+mongoose.connect(mongoUrl)
+  .then(() => console.log('Connected!'))
+  .catch((error) => {
+    console.log(error);
+  })
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
